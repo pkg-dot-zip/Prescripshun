6662f03f052b6f1830ed988e1e14e5a980054e7a
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-09-05 11:18:57 +0200

    Copied base UDP client-server

--- PrescripshunClient/Program.cs
+++ PrescripshunClient/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PrescripshunLib.Networking;
+
 Console.WriteLine("Hello, World!");
+
+
+UDPSocket c = new UDPSocket();
+c.Client("127.0.0.1", 27000);
+c.Send("TEST!");
+
+Console.ReadLine();
+c._socket.Close();
+Console.WriteLine("Closed client \n Press any key to exit");
+Console.ReadKey();
--- PrescripshunServer/Program.cs
+++ PrescripshunServer/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using PrescripshunLib.Networking;
+
+UDPSocket s = new UDPSocket();
+s.Server("127.0.0.1", 27000);
+
+Console.ReadKey();
+s._socket.Close();
+Console.WriteLine("Closed Server \n Press any key to exit");
+Console.ReadKey();
