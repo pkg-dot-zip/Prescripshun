81e61771e53f80acdaa6bf45637a9da727134cec
pkg-dot-zip <simon@meneerdegroot.nl>
2024-09-09 21:21:44 +0200

    Implemented new TCP system

--- PrescripshunClient/Client.cs
+++ PrescripshunClient/Client.cs
@@ -1,26 +1,81 @@
 ﻿using PrescripshunLib.Networking;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Unclassified.Net;
 
 namespace PrescripshunClient;
 
-internal class Client : IReceiveCallback
+internal class Client : AsyncTcpClient
 {
-    private static void Main(string[] args) => new Client().Boot();
-    private void Boot()
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-
-        UdpSocket c = new NetworkClient(NetworkHandler.IpAddress, NetworkHandler.Port, this);
-        c.Send("TEST!");
-
-        Console.ReadLine();
-        c.Socket.Close();
-        Console.WriteLine("Closed client \n Press any key to exit");
-        Console.ReadKey();
+        RunAsync().GetAwaiter().GetResult();
     }
 
-    public void OnReceive(ReceivedArgs args)
+    private static async Task RunAsync()
     {
-        Console.WriteLine("CLIENT RECEIVED: {0}: {1}, {2}", args.EndPointReceivedFrom, args.Bytes,
-            args.Text);
+        var port = NetworkHandler.Port;
+
+        var client = new AsyncTcpClient
+        {
+            IPAddress = IPAddress.IPv6Loopback,
+            Port = port,
+            //AutoReconnect = true,
+            ConnectedCallback = async (c, isReconnected) =>
+            {
+                await c.WaitAsync();   // Wait for server banner
+                await Task.Delay(50);   // Let the banner land in the console window
+                Console.WriteLine("Client: type a message at the prompt, or empty to quit (server shutdown in 10s)");
+                while (true)
+                {
+                    Console.Write("> ");
+                    var consoleReadCts = new CancellationTokenSource();
+                    var consoleReadTask = Console.In.ReadLineAsync(consoleReadCts.Token).AsTask();
+
+                    // Wait for user input or closed connection
+                    var completedTask = await Task.WhenAny(consoleReadTask, c.ClosedTask);
+                    if (completedTask == c.ClosedTask)
+                    {
+                        // Closed connection
+                        consoleReadCts.Cancel();
+                        break;
+                    }
+
+                    // User input
+                    string enteredMessage = await consoleReadTask;
+                    if (enteredMessage == "")
+                    {
+                        // Close the client connection
+                        c.Disconnect();
+                        break;
+                    }
+                    byte[] bytes = Encoding.UTF8.GetBytes(enteredMessage);
+                    await c.Send(new ArraySegment<byte>(bytes, 0, bytes.Length));
+
+                    // Wait for server response or closed connection
+                    await c.ByteBuffer.WaitAsync();
+                    if (c.IsClosing)
+                    {
+                        break;
+                    }
+                }
+                // NOTE: The client connection will NOT be closed automatically when this method
+                //       returns. It has to be closed explicitly when desired.
+            },
+            ReceivedCallback = (c, count) =>
+            {
+                byte[] bytes = c.ByteBuffer.Dequeue(count);
+                string message = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+                Console.WriteLine("Client: received: " + message);
+                return Task.CompletedTask;
+            }
+        };
+        client.Message += (s, a) => Console.WriteLine("Client: " + a.Message);
+        var clientTask = client.RunAsync();
+
+        await clientTask;
     }
 }
--- PrescripshunServer/Server.cs
+++ PrescripshunServer/Server.cs
@@ -1,31 +1,55 @@
 ﻿using PrescripshunLib.Networking;
+using System.Net;
+using System.Text;
+using Unclassified.Net;
 
 namespace PrescripshunServer;
 
-internal class Server : IReceiveCallback
+internal class Server : AsyncTcpClient
 {
-    private static void Main(string[] args) => new Server().Boot();
-
-    private NetworkServer _server = null!;
-
-    private void Boot()
+    private static void Main(string[] args)
     {
-        _server = new NetworkServer(NetworkHandler.IpAddress, NetworkHandler.Port, this);
-
-        Console.ReadKey();
-        _server.Socket.Close();
-        Console.WriteLine("Closed Server \n Press any key to exit");
-        Console.ReadKey();
+        RunAsync().GetAwaiter().GetResult();
     }
 
-    public void OnReceive(ReceivedArgs args)
+    private static async Task RunAsync()
     {
-        Console.WriteLine("SERVER RECEIVED: {0}: {1}, {2}", args.EndPointReceivedFrom, args.Bytes,
-            args.Text);
+        int port = NetworkHandler.Port;
 
-        if (args.Text.Equals("TEST!"))
+        var server = new AsyncTcpListener
         {
-            _server.SendTo("TEST WORKED!", args.EndPointReceivedFrom);
-        }
+            IPAddress = IPAddress.IPv6Any,
+            Port = port,
+            ClientConnectedCallback = tcpClient =>
+                new AsyncTcpClient
+                {
+                    ServerTcpClient = tcpClient,
+                    ConnectedCallback = async (serverClient, isReconnected) =>
+                    {
+                        await Task.Delay(500);
+                        byte[] bytes = Encoding.UTF8.GetBytes($"Hello, {tcpClient.Client.RemoteEndPoint}, my name is Server. Talk to me.");
+                        await serverClient.Send(new ArraySegment<byte>(bytes, 0, bytes.Length));
+                    },
+                    ReceivedCallback = async (serverClient, count) =>
+                    {
+                        byte[] bytes = serverClient.ByteBuffer.Dequeue(count);
+                        string message = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+                        Console.WriteLine("Server client: received: " + message);
+
+                        bytes = Encoding.UTF8.GetBytes("You said: " + message);
+                        await serverClient.Send(new ArraySegment<byte>(bytes, 0, bytes.Length));
+
+                        if (message == "bye")
+                        {
+                            // Let the server close the connection
+                            serverClient.Disconnect();
+                        }
+                    }
+                }.RunAsync()
+        };
+        server.Message += (s, a) => Console.WriteLine("Server: " + a.Message);
+        var serverTask = server.RunAsync();
+
+        await serverTask;
     }
 }
