7f463eb7cac58e80852a23f36f6466c1e70f5687
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-09-15 13:55:24 +0200

    Client console input now handled in separate method

--- PrescripshunClient/Client.cs
+++ PrescripshunClient/Client.cs
@@ -35,8 +35,8 @@ internal class Client : AsyncTcpClient
             // ON CONNECT:
             ConnectedCallback = async (c, isReconnected) =>
             {
-                await c.WaitAsync(); // Wait for server banner
-                await Task.Delay(50); // Let the banner land in the console window
+                await c.WaitAsync(); // Wait for server banner.
+                await Task.Delay(50); // Let the banner land in the console window.
                 Console.WriteLine("Client: type a message at the prompt, or empty to quit.");
                 while (true)
                 {
@@ -44,11 +44,11 @@ internal class Client : AsyncTcpClient
                     var consoleReadCts = new CancellationTokenSource();
                     var consoleReadTask = Console.In.ReadLineAsync(consoleReadCts.Token).AsTask();
 
-                    // Wait for user input or closed connection
+                    // Wait for user input or closed connection.
                     var completedTask = await Task.WhenAny(consoleReadTask, c.ClosedTask);
                     if (completedTask == c.ClosedTask)
                     {
-                        consoleReadCts.Cancel(); // Closed connection
+                        consoleReadCts.Cancel(); // Closed connection.
                         break;
                     }
 
@@ -56,38 +56,13 @@ internal class Client : AsyncTcpClient
                     string enteredMessage = await consoleReadTask;
                     if (enteredMessage == "")
                     {
-                        c.Disconnect(); // Close the client connection
+                        c.Disconnect(); // Close the client connection.
                         break;
                     }
 
-                    IMessage toSend = null;
+                    if (enteredMessage is not null) await HandleConsoleInput(c, enteredMessage);
 
-
-                    if (enteredMessage.StartsWith("1"))
-                    {
-                        Logger.Info($"Found {typeof(Message.MessageTest)} to send!");
-                        toSend = new Message.MessageTest()
-                        {
-                            IntegerTest = 1,
-                            DoubleTest = 2.0,
-                            FloatTest = 3.0F,
-                        };
-                    }
-                    else
-                    {
-                        Logger.Info($"Found {typeof(Message.DebugPrint)} to send!");
-                        toSend = new PrescripshunLib.Networking.Message.DebugPrint()
-                        {
-                            Text = enteredMessage
-                        };
-                    }
-
-
-                    if (toSend is null) throw new NullReferenceException();
-                    byte[] bytes = Encoding.UTF8.GetBytes(toSend.ToJsonString());
-                    await c.Send(new ArraySegment<byte>(bytes, 0, bytes.Length));
-
-                    // Wait for server response or closed connection
+                    // Wait for server response or closed connection.
                     await c.ByteBuffer.WaitAsync();
                     if (c.IsClosing) break;
                 }
@@ -110,6 +85,36 @@ internal class Client : AsyncTcpClient
         var clientTask = client.RunAsync();
 
         await clientTask;
+    }
+
+    private async Task HandleConsoleInput(AsyncTcpClient client, string enteredMessage)
+    {
+        IMessage toSend = null;
+
+
+        if (enteredMessage.StartsWith("1"))
+        {
+            Logger.Info($"Found {typeof(Message.MessageTest)} to send!");
+            toSend = new Message.MessageTest()
+            {
+                IntegerTest = 1,
+                DoubleTest = 2.0,
+                FloatTest = 3.0F,
+            };
+        }
+        else
+        {
+            Logger.Info($"Found {typeof(Message.DebugPrint)} to send!");
+            toSend = new PrescripshunLib.Networking.Message.DebugPrint()
+            {
+                Text = enteredMessage
+            };
+        }
+
+
+        if (toSend is null) throw new NullReferenceException();
+        byte[] bytes = Encoding.UTF8.GetBytes(toSend.ToJsonString());
+        await client.Send(new ArraySegment<byte>(bytes, 0, bytes.Length));
     }
 
     private void RegisterEvents()
