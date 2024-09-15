2d9368271d262b1803c05fe858b1f5b640328dc6
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-09-15 12:30:19 +0200

    Fixed GetMessageFromJsonString method

--- PrescripshunLib/Networking/BaseMessage.cs
+++ PrescripshunLib/Networking/BaseMessage.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace PrescripshunLib.Networking
 {
@@ -6,6 +7,12 @@ namespace PrescripshunLib.Networking
     {
         public abstract bool InitializeFromJsonString(string jsonString, out IMessage message);
 
-        public string ToJsonString() => JsonConvert.SerializeObject(this);
+        public string ToJsonString()
+        {
+            var jsonString = JsonConvert.SerializeObject(this);
+            var jsonObject = JObject.Parse(jsonString);             // Parse the serialized string into a JObject so we can modify it.
+            jsonObject.Add("type", this.GetType().Name);         // Add the "type" field with the name of the class.
+            return jsonObject.ToString(Formatting.None);
+        }
     }
 }
--- PrescripshunLib/Networking/Message.cs
+++ PrescripshunLib/Networking/Message.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Reflection;
 
 namespace PrescripshunLib.Networking
@@ -9,18 +10,57 @@ namespace PrescripshunLib.Networking
 
         public static IMessage GetMessageFromJsonString(string jsonString)
         {
-            var messages = GetAll();
-
-            foreach (var message in messages)
+            try
             {
-                if (message.InitializeFromJsonString(jsonString, out var message1))
-                {
-                    Logger.Info($"Deserialized message type found in {nameof(GetMessageFromJsonString)}: {message1.GetType()}");
-                    return message1;
-                }
-            }
+                // Parse the JSON string into a JObject to inspect its structure
+                var jsonObject = JObject.Parse(jsonString);
 
-            throw new ArgumentException("Invalid jsonString cannot be parsed into a message!");
+                // Check if there is a "Type" field to distinguish the message type
+                var typeToken = jsonObject["type"];
+                if (typeToken == null)
+                {
+                    throw new ArgumentException("Invalid JSON: No 'type' field found.");
+                }
+
+                // Get the type name from the "type" field
+                string messageType = typeToken.ToString();
+
+                Logger.Info($"Received message type: {messageType}");
+
+                // Find the matching message type from our known types
+                var messageClass = GetMessageTypeByName(messageType);
+                if (messageClass == null)
+                {
+                    throw new ArgumentException($"Unknown message type: {messageType}");
+                }
+
+                // Deserialize the JSON string into the correct message type
+                var deserializedMessage = (IMessage)JsonConvert.DeserializeObject(jsonString, messageClass);
+                if (deserializedMessage == null)
+                {
+                    throw new JsonException("Failed to deserialize JSON into the correct message type.");
+                }
+
+                Logger.Info($"Successfully deserialized message of type {deserializedMessage.GetType().Name}");
+                return deserializedMessage;
+            }
+            catch (JsonException ex)
+            {
+                Logger.Error(ex, "Failed to parse JSON string.");
+                throw;
+            }
+        }
+
+        // This method finds the message class by its type name
+        private static Type GetMessageTypeByName(string messageType)
+        {
+            // Get all the message types that inherit from BaseMessage
+            var assembly = Assembly.GetExecutingAssembly();
+            var messageTypes = assembly.GetTypes()
+                .Where(t => typeof(IMessage).IsAssignableFrom(t) && t is { IsAbstract: false, IsInterface: false });
+
+            // Find the type that matches the name
+            return messageTypes.FirstOrDefault(t => t.Name.Equals(messageType, StringComparison.OrdinalIgnoreCase));
         }
 
         private static List<BaseMessage> GetAll()
