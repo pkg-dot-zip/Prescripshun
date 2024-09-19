972250c4f78f227e46c5eb416c92dbad2499048a
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-09-19 11:01:14 +0200

    Added classes of util package

---
+++ PrescripshunLib/Util/Sound/Beeper.cs
@@ -0,0 +1,17 @@
+﻿namespace PrescripshunLib.Util.Sound
+{
+    public static class Beeper
+    {
+        // TODO: Implement.
+        public static void PlayServerBootSound()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement.
+        public static Task PlayServerBootSoundAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PrescripshunLib/Util/Sound/SoundHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace PrescripshunLib.Util.Sound
+{
+    public static class SoundHandler
+    {
+        // TODO: Implement.
+        public static void PlaySound(string soundFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement.
+        public static Task PlaySoundAsync(string soundFilePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
