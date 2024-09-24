47fa808f454d597b6b99bdbb214617219ffe1446
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-09-24 16:58:26 +0200

    Added docs for SqlDatabase

--- PrescripshunServer/Database/MySql/SqlDatabase.cs
+++ PrescripshunServer/Database/MySql/SqlDatabase.cs
@@ -6,12 +6,18 @@ using SqlKata.Compilers;
 namespace PrescripshunServer.Database.MySql
 {
     /// <summary>
+    /// Represents a MySQL database connection manager that handles database connection,
+    /// disconnection, and query execution for the Prescripshun server.
+    /// This class provides methods to connect to a MySQL database, execute queries, 
+    /// and check the existence of tables and data.
     /// 
+    /// Note: The database specified in the constructor must already exist as this class
+    /// does not create it. The default configuration assumes no password is set for the MySQL connection.
     /// </summary>
-    /// <param name="server"></param>
-    /// <param name="port"></param>
-    /// <param name="dbName">Name of database to connect to. NOTE: This database has to exist before launching the server as we do not create it!</param>
-    /// <param name="username"></param>
+    /// <param name="server">The server address of the MySQL database (default is 'localhost').</param>
+    /// <param name="port">The port number for the MySQL connection (default is 3306).</param>
+    /// <param name="dbName">The name of the MySQL database to connect to. This database must exist prior to launching the server.</param>
+    /// <param name="username">The username used to authenticate with the MySQL database (default is 'root').</param>
     internal class SqlDatabase(
         string server = "localhost",
         int port = 3306,
@@ -23,6 +29,12 @@ namespace PrescripshunServer.Database.MySql
 
         private readonly MySqlConnection _myConn = new($"Server={server};Port={port};Database={dbName};Uid={username};SslMode=None"); // NOTE: Assumes there is no password.
 
+        /// <summary>
+        /// Establishes a connection to the MySQL database using the provided connection parameters.
+        /// Logs a success message upon a successful connection, or logs and throws an exception if the connection fails.
+        /// </summary>
+        /// <exception cref="MySqlException">Thrown if there is an error during the connection attempt to the database.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if the connection is already open or in an invalid state.</exception>
         public void Connect()
         {
             try
@@ -37,6 +49,13 @@ namespace PrescripshunServer.Database.MySql
             }
         }
 
+        /// <summary>
+        /// Asynchronously establishes a connection to the MySQL database using the provided connection parameters.
+        /// Logs a success message upon a successful connection, or logs and throws an exception if the connection attempt fails.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation of opening the database connection.</returns>
+        /// <exception cref="MySqlException">Thrown if there is an error during the asynchronous connection attempt to the database.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if the connection is already open or in an invalid state.</exception>
         public async Task ConnectAsync()
         {
             try
@@ -51,6 +70,13 @@ namespace PrescripshunServer.Database.MySql
             }
         }
 
+        /// <summary>
+        /// Disconnects from the MySQL database if the connection is currently open. 
+        /// Logs a success message upon a successful disconnection, or logs if the connection was already closed.
+        /// </summary>
+        /// <returns>Returns <c>true</c> if the connection was open and successfully closed, 
+        /// or <c>false</c> if the connection was already closed.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the connection is in an invalid state and cannot be closed.</exception>
         public bool Disconnect()
         {
             switch (_myConn.State)
@@ -67,6 +93,14 @@ namespace PrescripshunServer.Database.MySql
                     throw new InvalidOperationException();
             }
         }
+
+        /// <summary>
+        /// Asynchronously disconnects from the MySQL database if the connection is currently open. 
+        /// Logs a success message upon a successful disconnection, or logs if the connection was already closed.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation, returning <c>true</c> if the connection was open and successfully closed, 
+        /// or <c>false</c> if the connection was already closed.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the connection is in an invalid state and cannot be closed.</exception>
         public async Task<bool> DisconnectAsync()
         {
             switch (_myConn.State)
@@ -90,6 +124,15 @@ namespace PrescripshunServer.Database.MySql
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Executes the provided SQL query and processes the result using the specified action.
+        /// Logs the query being executed, and invokes the given action with the resulting <see cref="MySqlDataReader"/>.
+        /// </summary>
+        /// <param name="query">The SQL query to be executed.</param>
+        /// <param name="action">The action to be performed on the <see cref="MySqlDataReader"/> containing the query results.</param>
+        /// <exception cref="MySqlException">Thrown if there is an error executing the SQL query.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if the connection state is invalid for executing the query.</exception>
+        /// <exception cref="Exception">Thrown for any general errors during query execution.</exception>
         public void ExecuteQuery(string query, Action<MySqlDataReader> action)
         {
             Logger.Info($"Executing SQL Query: {query}");
@@ -107,6 +150,14 @@ namespace PrescripshunServer.Database.MySql
             }
         }
 
+        /// <summary>
+        /// Executes the provided SQL non-query command, such as an INSERT, UPDATE, DELETE, or DDL statement.
+        /// Logs the command being executed, and performs the operation without returning any result set.
+        /// </summary>
+        /// <param name="nonQuery">The SQL non-query command to be executed.</param>
+        /// <exception cref="MySqlException">Thrown if there is an error executing the SQL non-query.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if the connection state is invalid for executing the non-query.</exception>
+        /// <exception cref="Exception">Thrown for any general errors during non-query execution.</exception>
         public void ExecuteNonQuery(string nonQuery)
         {
             Logger.Info($"Executing SQL NonQuery: {nonQuery}");
@@ -122,6 +173,15 @@ namespace PrescripshunServer.Database.MySql
             }
         }
 
+        /// <summary>
+        /// Asynchronously executes the provided SQL non-query command, such as an INSERT, UPDATE, DELETE, or DDL statement.
+        /// Logs the command being executed, and performs the operation without returning any result set.
+        /// </summary>
+        /// <param name="nonQuery">The SQL non-query command to be executed.</param>
+        /// <returns>A task representing the asynchronous execution of the non-query command.</returns>
+        /// <exception cref="MySqlException">Thrown if there is an error executing the SQL non-query.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if the connection state is invalid for executing the non-query.</exception>
+        /// <exception cref="Exception">Thrown for any general errors during non-query execution.</exception>
         public async Task ExecuteNonQueryAsync(string nonQuery)
         {
             Logger.Info($"Executing SQL NonQuery: {nonQuery}");
@@ -137,6 +197,14 @@ namespace PrescripshunServer.Database.MySql
             }
         }
 
+        /// <summary>
+        /// Checks if a table with the specified name exists in the current MySQL database.
+        /// Logs the table name being checked and executes a query against the information schema to determine its existence.
+        /// </summary>
+        /// <param name="tableName">The name of the table to check for existence.</param>
+        /// <returns>Returns <c>true</c> if the table exists, otherwise <c>false</c>.</returns>
+        /// <exception cref="MySqlException">Thrown if there is an error executing the SQL query to check for the table's existence.</exception>
+        /// <exception cref="Exception">Thrown for any general errors during the existence check.</exception>
         public bool TableExists(string tableName)
         {
             string query = $"SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = '{tableName}';";
@@ -154,6 +222,14 @@ namespace PrescripshunServer.Database.MySql
             }
         }
 
+        /// <summary>
+        /// Checks if the specified table contains any data by counting the number of rows.
+        /// Logs the table name being checked and executes a query to determine if any records exist.
+        /// </summary>
+        /// <param name="tableName">The name of the table to check for data.</param>
+        /// <returns>Returns <c>true</c> if the table contains data, otherwise <c>false</c>.</returns>
+        /// <exception cref="MySqlException">Thrown if there is an error executing the SQL query to check for table data.</exception>
+        /// <exception cref="Exception">Thrown for any general errors during the data check.</exception>
         public bool TableHasData(string tableName)
         {
             string query = $"SELECT COUNT(*) FROM {tableName};";
