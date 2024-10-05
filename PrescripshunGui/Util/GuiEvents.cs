bba27ce7409ebf57150bdd676750776c93cadfb1
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-10-05 17:05:40 +0200

    MessageBox related code now delegated to MessageBoxHandler

--- PrescripshunGui/Util/GuiEvents.cs
+++ PrescripshunGui/Util/GuiEvents.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Logging;
-using Avalonia.Threading;
-using MsBox.Avalonia;
-using MsBox.Avalonia.Enums;
+﻿using System.Threading.Tasks;
 using PrescripshunClient;
 using PrescripshunLib.Networking.Messages;
 
@@ -16,44 +9,26 @@ namespace PrescripshunGui.Util
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         public static ClientEvents Get() => ClientEvents.Get;
 
-        private static Window? GetCurrentOwner()
-        {
-            if (Avalonia.Application.Current is null)
-            {
-                Logger.Trace("Returning null in {0} because no current application running.", nameof(GetCurrentOwner));
-                return null;
-            }
-
-            if (Avalonia.Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                return desktop.MainWindow;
-            }
-
-            throw new NotImplementedException("No support for other platforms but desktop have been implemented!");
-        }
-
         public static void RegisterEvents()
         {
             NetworkHandler.Client.RegisterEvents();
             Logger.Info("Registering events in {0}", nameof(GuiEvents));
 
-            Get().OnReceiveMessage.AddHandler<LoginResponse>(async (client, message) =>
+            Get().OnReceiveMessage.AddHandler<LoginResponse>((client, message) =>
             {
                 if (message.IsValid())
                 {
                     Logger.Info("Logged in as {0}", message.Id);
-                    return;
+                    return Task.CompletedTask;
                 }
 
                 Logger.Info("Login attempt FAILED!");
-                string title = "Login failed!";
-                string description = message.Reason;
+                return Task.CompletedTask;
+            });
 
-                await Dispatcher.UIThread.InvokeAsync(async () =>
-                {
-                    var box = MessageBoxManager.GetMessageBoxStandard(title, description);
-                    await box.ShowWindowDialogAsync(GetCurrentOwner());
-                });
+            Get().OnReceiveMessage.AddHandler<LoginResponse>(async (client, message) =>
+            {
+                if (!message.IsValid()) await MessageBoxHandler.SimplePopUp("Login Failed!", message.Reason);
             });
         }
     }
