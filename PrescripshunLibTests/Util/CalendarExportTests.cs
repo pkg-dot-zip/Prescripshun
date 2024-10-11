49bce95e97d9127e38615542f3772ce9d41f1b0e
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-10-11 14:00:14 +0200

    Extended unit tests

--- PrescripshunLibTests/Util/CalendarExportTests.cs
+++ PrescripshunLibTests/Util/CalendarExportTests.cs
@@ -6,9 +6,6 @@ namespace PrescripshunLib.Util.Tests;
 [TestClass]
 public class CalendarExportTests
 {
-
-    // TODO: Create unit tests with special chars: comma (,), period(.), exclamation mark (!) and question mark(?).
-
     [TestMethod]
     [DataRow("Title1")]
     [DataRow("Title2")]
@@ -16,7 +13,101 @@ public class CalendarExportTests
     [DataRow("Titel1")]
     [DataRow("Titel2")]
     [DataRow("Titel3")]
+    [DataRow("")]
     public void GetGoogleCalenderAppointmentsExportString_SimpleChars_CanParseTitleCorrectly_ReturnsTrue(string title, string description = "Beschrijving1", string dateTimeString = "01-01-1999")
+    {
+        List<Appointment> appointments = [new Appointment()
+        {
+            Title = title,
+            Description = description,
+            DateTime = DateTime.Parse(dateTimeString)
+        }];
+
+        var exportString = CalendarExport.GetGoogleCalenderAppointmentsExportString(appointments);
+
+        Assert.AreEqual($"Subject,Start Date,Start Time,End Date,End Time,Description\r\n{title},01/01/1999,12:00 AM,01/01/1999,12:15 AM,Beschrijving1\r\n", exportString);
+    }
+
+    [TestMethod]
+    [DataRow(",Title1")]
+    [DataRow("T,itle2")]
+    [DataRow("Ti,tle3")]
+    [DataRow("Tit,el1")]
+    [DataRow("Tite,l2")]
+    [DataRow("Titel,3")]
+    [DataRow("Titel4,")]
+    [DataRow(",,,,,,,")]
+    public void GetGoogleCalenderAppointmentsExportString_ContainsComma_CanParseTitleCorrectly_ReturnsTrue(string title, string description = "Beschrijving1", string dateTimeString = "01-01-1999")
+    {
+        List<Appointment> appointments = [new Appointment()
+        {
+            Title = title,
+            Description = description,
+            DateTime = DateTime.Parse(dateTimeString)
+        }];
+
+        var exportString = CalendarExport.GetGoogleCalenderAppointmentsExportString(appointments);
+
+        // If a value contains a comma, quotations should've been inserted around that value.
+        Assert.AreEqual($"Subject,Start Date,Start Time,End Date,End Time,Description\r\n\"{title}\",01/01/1999,12:00 AM,01/01/1999,12:15 AM,Beschrijving1\r\n", exportString);
+    }
+
+    [TestMethod]
+    [DataRow(".Title1")]
+    [DataRow("T.itle2")]
+    [DataRow("Ti.tle3")]
+    [DataRow("Tit.el1")]
+    [DataRow("Tite.l2")]
+    [DataRow("Titel.3")]
+    [DataRow("Titel4.")]
+    [DataRow(".......")]
+    public void GetGoogleCalenderAppointmentsExportString_ContainsPeriod_CanParseTitleCorrectly_ReturnsTrue(string title, string description = "Beschrijving1", string dateTimeString = "01-01-1999")
+    {
+        List<Appointment> appointments = [new Appointment()
+        {
+            Title = title,
+            Description = description,
+            DateTime = DateTime.Parse(dateTimeString)
+        }];
+
+        var exportString = CalendarExport.GetGoogleCalenderAppointmentsExportString(appointments);
+
+        Assert.AreEqual($"Subject,Start Date,Start Time,End Date,End Time,Description\r\n{title},01/01/1999,12:00 AM,01/01/1999,12:15 AM,Beschrijving1\r\n", exportString);
+    }
+
+    [TestMethod]
+    [DataRow("?Title1")]
+    [DataRow("T?itle2")]
+    [DataRow("Ti?tle3")]
+    [DataRow("Tit?el1")]
+    [DataRow("Tite?l2")]
+    [DataRow("Titel?3")]
+    [DataRow("Titel4?")]
+    [DataRow("???????")]
+    public void GetGoogleCalenderAppointmentsExportString_ContainsQuestionMark_CanParseTitleCorrectly_ReturnsTrue(string title, string description = "Beschrijving1", string dateTimeString = "01-01-1999")
+    {
+        List<Appointment> appointments = [new Appointment()
+        {
+            Title = title,
+            Description = description,
+            DateTime = DateTime.Parse(dateTimeString)
+        }];
+
+        var exportString = CalendarExport.GetGoogleCalenderAppointmentsExportString(appointments);
+
+        Assert.AreEqual($"Subject,Start Date,Start Time,End Date,End Time,Description\r\n{title},01/01/1999,12:00 AM,01/01/1999,12:15 AM,Beschrijving1\r\n", exportString);
+    }
+
+    [TestMethod]
+    [DataRow("!Title1")]
+    [DataRow("T!itle2")]
+    [DataRow("Ti!tle3")]
+    [DataRow("Tit!el1")]
+    [DataRow("Tite!l2")]
+    [DataRow("Titel!3")]
+    [DataRow("Titel4!")]
+    [DataRow("!!!!!!!")]
+    public void GetGoogleCalenderAppointmentsExportString_ContainsExclamationMark_CanParseTitleCorrectly_ReturnsTrue(string title, string description = "Beschrijving1", string dateTimeString = "01-01-1999")
     {
         List<Appointment> appointments = [new Appointment()
         {
@@ -37,6 +128,7 @@ public class CalendarExportTests
     [DataRow("Beschrijving1")]
     [DataRow("Beschrijving2")]
     [DataRow("Beschrijving3")]
+    [DataRow("")]
     public void GetGoogleCalenderAppointmentsExportString_SimpleChars_CanParseDescriptionCorrectly_ReturnsTrue(string description, string title = "Titel1", string dateTimeString = "01-01-1999")
     {
         List<Appointment> appointments = [new Appointment()
