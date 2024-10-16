ba43e424744598aebf8a0c1ca86e61b73becae7e
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-10-16 14:46:51 +0200

    Added docs to GenericEvent

--- PrescripshunLib/GenericEvent.cs
+++ PrescripshunLib/GenericEvent.cs
@@ -5,8 +5,7 @@ using Unclassified.Net;
 namespace PrescripshunLib;
 
 // NOTE: FIXING THIS HAS A VERY LOW PRIORITY SINCE THIS IS NOT TECHNICALLY NEEDED FOR THIS PROJECT, ONLY IF WE WANT TO REUSE THIS IN FUTURE PROJECTS.
-// TODO: Fix. The hardcoded delegate kinda defeats the purpose of this class being generic since the params other than of type T are hardcoded.
-// TODO: After fixing the above problem, fix the docs so it fits the generic state of this class.
+// TODO: Fix. The hardcoded delegate kinda defeats the purpose of this class being generic since the params other than of type T are hardcoded. In other words, don't hardcode AsyncTcpClient as param.
 
 /// <summary>
 /// Container for a generic event.
@@ -37,14 +36,25 @@ public class GenericEvent<T>
 
     private readonly Dictionary<Type, List<IEventDelegate>> _handlers = [];
 
+    /// <summary>
+    /// Adds the <paramref name="handler"/> from this <typeparamref name="TEventType"/>.
+    /// </summary>
+    /// <typeparam name="TEventType"><see langword="type"/> of <c>T</c> to register <paramref name="handler"/> for.</typeparam>
+    /// <param name="handler">Handler to add.</param>
     public void AddHandler<TEventType>(Handler<TEventType> handler) where TEventType : T
     {
         var messageType = typeof(TEventType);
         Logger.Info("Adding handle for {0}", messageType.Name);
         if (!_handlers.ContainsKey(messageType)) _handlers.Add(messageType, []);
-        _handlers[messageType].Add(new EventDelegate<TEventType> { Implementation = handler });
+        _handlers[messageType].Add(new EventDelegate<TEventType> {Implementation = handler});
     }
 
+    /// <summary>
+    /// Removes the <paramref name="handler"/> from this <typeparamref name="TEventType"/>.
+    /// </summary>
+    /// <typeparam name="TEventType"><see langword="type"/> of <c>T</c> to delete <paramref name="handler"/> for.</typeparam>
+    /// <param name="handler">Handler to remove.</param>
+    /// <returns><c>true</c> if any handler(s) was removed. <c>false</c> if no handlers were removed.</returns>
     public bool RemoveHandler<TEventType>(Handler<TEventType> handler) where TEventType : T
     {
         var messageType = typeof(TEventType);
@@ -58,6 +68,16 @@ public class GenericEvent<T>
         return nofRemoved != 0;
     }
 
+    /// <summary>
+    /// Asynchronously invokes all handlers registered for <typeparamref name="T"/> with <paramref name="message"/>.
+    /// We pass an instance of <seealso cref="AsyncTcpClient"/> so that both the server and client can
+    /// send an instance of <typeparamref name="T"/> back if needed.
+    ///
+    /// Please note that <seealso cref="Task.WhenAll(Task[])"/> is used here.
+    /// </summary>
+    /// <param name="serverClient">Instance of <see cref="AsyncTcpClient"/> so that handlers can send a message of type <typeparamref name="T"/> back.</param>
+    /// <param name="message">Instance of <typeparamref name="T"/> to invoke all <see cref="_handlers"/> with.</param>
+    /// <returns></returns>
     public async Task Invoke(AsyncTcpClient serverClient, T message)
     {
         Debug.Assert(message != null, nameof(message) + " != null");
